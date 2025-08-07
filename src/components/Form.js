6b95e6248fc389657152eda64a5ab34945209260
Yagaven <yagaad118@rmkcet.ac.in>
2025-08-07 18:13:54 +0530

    INPUT FETCHING

---
+++ src/components/Form.js
@@ -0,0 +1,7 @@
+import React,{useState} from "react";
+
+function Form() {
+    const[username, setUsername] = useState("");
+    const[email, setEmail] = useState("");
+    const[password, setPassword] = useState("");        
+}
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,16 +2,37 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../index.css';
 function Login() {
+
+  const [username, setUsername] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handleLogin = () => {
+    console.log('login Info:',{username, email, password});
+  }
     return(
     <div>
     <div className="container">
       <div className="title">DOCUMENT LIFE TRACKER</div>
       <div className="section-title">LOGIN</div>
-      <input type="text" placeholder="USER NAME" />
-      <input type="email" placeholder="EMAIL" />
-      <input type="password" placeholder="PASSWORD" />
+      <input type="text"
+      value={username}
+      onChange={(e) => setUsername(e.target.value)}
+      placeholder="USER NAME" />
+
+      <input type="email"
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+       id = "email"placeholder="EMAIL" />
+
+
+      <input type="password"
+      value={password}
+      onChange={(e) => setPassword(e.target.value)}
+       id="password"placeholder="PASSWORD" />
       <Link to="/Tracker">
-        <button type="button">Submit</button>
+        <button type="button"
+        onClick={handleLogin}>Submit</button>
       </Link> 
     </div>
     <div className="logout">
