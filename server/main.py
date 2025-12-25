be3203d49bcad43537eaa5c92d47e704a5b49832
YashikaKannan <yashikakannan05@gmail.con>
2025-12-26 00:29:29 +0530

    host link added

--- server/main.py
+++ server/main.py
@@ -25,7 +25,7 @@ def _start_scheduler_once():
 # CORS middleware setup
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # frontend URL
+    allow_origins=["https://document-life-tracker.vercel.app", "http://localhost:3000"],  # frontend URL
     allow_credentials=True,
     allow_methods=["*"],  # allow all HTTP methods
     allow_headers=["*"],  # allow all headers
